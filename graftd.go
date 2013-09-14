908abf7ef421b22cff34657db1d3a2f0820a65f7
benmills <ben@benmills.org>
2013-09-13 19:49:45 -0700

    Leader election and basic append entry.

--- graftd.go
+++ graftd.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-	"log"
-	"os"
 	"flag"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 
-	"github.com/dev-urandom/graft"
 	"github.com/bmizerany/pat"
+	"github.com/dev-urandom/graft"
 )
 
 var (
-	logger *log.Logger
-	port string
-	host string
-	server *graft.Server
+	logger        *log.Logger
+	port          string
+	host          string
+	server        *graft.Server
 	serverStarted bool
 )
 
@@ -23,13 +24,14 @@ func init() {
 	flag.StringVar(&host, "host", "localhost", "host")
 	flag.Parse()
 
-	server = graft.New()
+	server = graft.New(host + ":" + port)
 	serverStarted = false
 	logger = log.New(os.Stdin, "["+host+":"+port+"] ", log.LstdFlags)
 }
 
 func main() {
 	http.Handle("/", routes(server))
+	http.Handle("/raft/", graft.PrefixedHttpHandler(server, "/raft"))
 
 	logger.Println("Listening...")
 
@@ -43,18 +45,31 @@ func routes(server *graft.Server) http.Handler {
 	handle := pat.New()
 
 	handle.Get("/status", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		res := Response{w, r}
+		res := NewResponse(w, r)
+
+		res.logging = false
 
 		res.respond(200, JsonData{
-			"id": server.Id,
-			"term": server.Term,
-			"votedFor": server.VotedFor,
-			"state": server.State,
+			"id":              server.Id,
+			"term":            server.Term,
+			"votedFor":        server.VotedFor,
+			"state":           server.State,
+			"lastCommitIndex": server.LastCommitIndex(),
+			"lastLogIndex":    server.LastLogIndex(),
+			"peers":           server.Peers,
+		})
+	}))
+
+	handle.Get("/log", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res := NewResponse(w, r)
+
+		res.respond(200, JsonData{
+			"log": server.Log,
 		})
 	}))
 
 	handle.Post("/start", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		res := Response{w, r}
+		res := NewResponse(w, r)
 
 		if !serverStarted {
 			server.Start()
@@ -64,6 +79,32 @@ func routes(server *graft.Server) http.Handler {
 		} else {
 			res.respond(400, JsonData{"message": "server already started"})
 		}
+	}))
+
+	handle.Post("/start_election", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res := NewResponse(w, r)
+		server.StartElection()
+		res.respond(200, JsonData{"message": "started election"})
+	}))
+
+	handle.Post("/append_entry", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res := NewResponse(w, r)
+		body, _ := ioutil.ReadAll(r.Body)
+		newEntry := string(body)
+
+		server.AppendEntries(newEntry)
+
+		res.respond(201, JsonData{"message": "commited to log"})
+	}))
+
+	handle.Post("/add_peer", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res := NewResponse(w, r)
+		body, _ := ioutil.ReadAll(r.Body)
+		newPeerURL := string(body)
+
+		server.AddPeers(graft.HttpPeer{"http://" + newPeerURL + "/raft"})
+
+		res.respond(201, JsonData{"message": "peer added"})
 	}))
 
 	return handle
--- response.go
+++ response.go
@@ -6,12 +6,19 @@ import (
 )
 
 type Response struct {
-	w http.ResponseWriter
-	r *http.Request
+	w       http.ResponseWriter
+	r       *http.Request
+	logging bool
+}
+
+func NewResponse(w http.ResponseWriter, r *http.Request) Response {
+	return Response{w, r, true}
 }
 
 func (res Response) respond(status int, data JsonData) {
 	res.w.WriteHeader(status)
 	io.WriteString(res.w, data.Encode())
-	logger.Println(res.r.Method, res.r.URL.Path, status, data.Encode())
+	if res.logging {
+		logger.Println(res.r.Method, res.r.URL.Path, status, data.Encode())
+	}
 }
